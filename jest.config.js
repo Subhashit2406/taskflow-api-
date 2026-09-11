4f70d1b2e7e4c62179a6de7818f6826c3df855f4
Subhashit2406 <subhashit.pathak.23cse@bmu.edu.in>
2026-09-11 15:46:10 +0530

    chore: project scaffolding, configuration, and dependencies

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  verbose: true,
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/server.js',
+    '!src/config/database.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'clover'],
+  setupFilesAfterEnv: ['./tests/setup.js'],
+  testTimeout: 10000
+};
